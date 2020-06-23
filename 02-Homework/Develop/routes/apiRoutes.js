e6f60d15398bd3d20c70039c88c995c6aa5298e9
AlkaTone <antonio.a.garces@gmail.com>
2020-06-23 10:25:13 -0700

    app done

---
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const db = require("../models/index.js");
+const router = require("express").Router();
+
+  
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  router.put("/api/workouts/:id", (req, res) => {
+    const {body, params} = req;
+    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .populate("exercises")
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+
+  });
+
+  module.exports = router;
---
+++ 02-Homework/Develop/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+// const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+
+// const db = require("./models/index.js");
+const apiRoutes = require("./routes/apiRoutes.js")
+const htmlRoutes = require("./routes/htmlRoutes.js");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+const options =  {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(MONGODB_URI, options);
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
